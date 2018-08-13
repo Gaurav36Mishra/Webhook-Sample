51c12e38c9397eb60b6d49e376b056d5a58fe830
Gaurav36Mishra <gaurav05mishra@gmail.com>
2018-08-13 15:00:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: speech,
-    displayText: speech,
+    speech: "Thank you for talking to HDFC Life. Our agent will talk to you soon",
+    displayText: "Thank you for talking to HDFC Life. Our agent will talk to you soon",
     source: "webhook-echo-sample"
   });
 });
